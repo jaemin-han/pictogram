82730bd8b7e6598059d5042df85b481f7a7543c8
jaemin.han <jaemin.han@nyu.edu>
2018-04-08 23:51:13 -0400

    json data response body list view connect

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const superagent = require('superagent')
 
 router.get('/', (req, res) => {
 
@@ -11,6 +12,33 @@ router.get('/', (req, res) => {
 	}
 
 	res.render('index', data)
+})
+
+router.get('/:username', (req, res) => {
+	const username = req.params.username
+	const instagramAPI = 'https://www.instagram.com/'+username+'/?__a=1'
+
+	superagent.get(instagramAPI)
+	.query(null)
+	.set('Accept', 'application/json')
+	.end((err, response) => {
+		if (err){
+			res.json({
+				confirmation: 'fail',
+				message: err.message
+			})
+			return
+		}
+
+		// res.render('index', response.body)
+
+		res.json({
+			confirmation: 'success',
+			// username: username
+			data: response.body
+		})
+	})		
+
 })
 
 module.exports = router
